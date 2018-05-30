a98ecac45439b7c72383d2cfff24343ccef765fc
tomejzen <tomejzen@wp.pl>
2018-05-30 19:49:54 +0200

    changed data labels

--- CoinTrends/scripts/site.js
+++ CoinTrends/scripts/site.js
@@ -6,15 +6,21 @@
     let myChart = chartController.InitializeChart('myChart');
 
     let ccHistoricalData = historicalDataProvider.LoadCryptocurrenciesHistoricalData(function (coinName, fetchedData) {
-
-        console.log(coinName);
-        console.log(fetchedData);
-
-        let labels = fetchedData.map(o => o.time);
-        let data = fetchedData.map(o => [o.average]);
+        
+        let labels = fetchedData.map(o => o.time.substring(0, o.time.length - 9)).reverse();
+        let data = fetchedData.map(o => o.average).reverse();
         let color = chartController.GetColor();
 
-        myChart.data.labels = labels;
+        if (labels.length > myChart.data.labels.length) {
+            let prependAmount = labels.length - myChart.data.labels.length;
+            myChart.data.labels = labels;
+
+            myChart.data.datasets.forEach((dataset) => {
+                let emptyArray = new Array(prependAmount).fill(undefined);
+                dataset.data = emptyArray.concat(dataset.data);
+            });
+        }
+
         myChart.data.datasets.push({
             label: coinName,
             data: data,
