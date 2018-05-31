ceba27c5c7ad641ed871b9dc033ea7b13aa12c7a
tomejzen <tomejzen@wp.pl>
2018-05-31 15:20:38 +0200

    created tests for extremes calculation

--- CoinTrends/scripts/Trends.js
+++ CoinTrends/scripts/Trends.js
@@ -6,11 +6,38 @@
 
     CalculateTrend(data, valueField) {
 
+        let minimums = [0];
+        let maximums = [0];
+
+        let direction = '';
+        let lastValue = data[0][valueField];
+
+        for (let i = 1; i < data.length; i++) {
+
+            let v = data[i][valueField];
+            let currentDirection = (v - lastValue) > 0 ? 'up' : 'down';
+
+            if (currentDirection != direction) {
+
+                if (currentDirection == 'up')
+                    minimums.push(i);
+                else
+                    maximums.push(i);
+            }
+
+            lastValue = v;
+            direction = currentDirection;
+        }
+        
         return {
             type: 'growth',
             startValue: 0,
             endValue: 0
         };
+    }
+
+    GetAllExtremes(data, valueField) {
+
     }
 
 }
--- CoinTrends/test/TrendsTests.js
+++ CoinTrends/test/TrendsTests.js
@@ -1,43 +1,58 @@
 ﻿
 describe('Trends', function () {
 
+    let data1 = [
+        {
+            date: "2018-05-10",
+            value: 0
+        },
+        {
+            date: "2018-05-11",
+            value: 1
+        },
+        {
+            date: "2018-05-12",
+            value: -1
+        },
+        {
+            date: "2018-05-13",
+            value: 2
+        },
+        {
+            date: "2018-05-14",
+            value: 0
+        },
+        {
+            date: "2018-05-15",
+            value: -1
+        },
+        {
+            date: "2018-05-16",
+            value: 4
+        },
+    ];
+
+    it('should calculate extremes', function () {
+
+        // Arrange
+        let trends = new Trends();
+
+        // Act
+        let extremes = trends.GetAllExtremes(data1, 'value');
+
+        // Assert
+        expect(extremes.minimums.length).toEqual(3);
+        expect(extremes.maximums.length).toEqual(4);
+
+    });
+
     it('should calculate growth trend', function () {
 
         // Arrange
         let trends = new Trends();
-        let data = [
-            {
-                date: "2018-05-10",
-                value: 0
-            },
-            {
-                date: "2018-05-11",
-                value: 1
-            },
-            {
-                date: "2018-05-12",
-                value: -1
-            },
-            {
-                date: "2018-05-13",
-                value: 2
-            },
-            {
-                date: "2018-05-14",
-                value: 0
-            },
-            {
-                date: "2018-05-15",
-                value: -1
-            },
-            {
-                date: "2018-05-16",
-                value: 4
-            },
-        ];
 
         // Act
-        let trend = trends.CalculateTrend(data, 'value');
+        let trend = trends.CalculateTrend(data1, 'value');
 
         // Assert
         expect(trend.type).toEqual('growth');
